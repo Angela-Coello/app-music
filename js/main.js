c0cb367d5d8353d772f86f118f610ddfc76b02cb
carmenb2020 <carmenborjap@hotmail.com>
2023-06-02 20:07:04 +0200

    add function playSound

--- js/main.js
+++ js/main.js
@@ -1,51 +1,33 @@
-let kick = document.getElementById('kick');
-kick.addEventListener('click', (e) => {
-    e.preventDefault();
-    const audio = new Audio('media/sounds/kick.wav');
-    audio.play();
-})
+/*Asegurarse de que el DOM está cargado*/
+document.addEventListener("DOMContentLoaded", function(e) {
+    /*
+      Seleccionamos todos los elementos del tipo rect
+    */
+    let theSound = document.querySelectorAll("rect");
+  
+    /*
+       theSound es un arreglo que contiene todos los elementos con el tipo rect,
+       los recorremos y aplicamos un listener que será la función playSound
+    */
+  
+    theSound.forEach(function(el, idx) {
+      el.addEventListener("click", playSound, false)
+    });
+  
+});
+  
+  /*
+      Usamos this+propiedad  
+      para obtener lo que deseemos del elemento
+      aquí obtenemos por ejemplo el id
+  */
+    function playSound(e) {
+        e.preventDefault();
+        const audio = new Audio(`media/sounds/${this.id}.wav`);
+        audio.play();
+    }
 
-let tom3 = document.getElementById('tom3');
-tom3.addEventListener('click', (e) => {
-    e.preventDefault();
-    const audio = new Audio('media/sounds/tom3.wav');
-    audio.play();
-})
 
-let hihat = document.getElementById("hihat");
-hihat.addEventListener('click', (e) => {
-    e.preventDefault();
-    const audio = new Audio('media/sounds/hihat.wav');
-    audio.play();
-})
-
-let tom1 = document.getElementById("tom1");
-tom1.addEventListener('click', (e) => {
-    e.preventDefault();
-    const audio = new Audio('media/sounds/tom1.wav');
-    audio.play();
-})
-
-let tom2 = document.getElementById("tom2");
-tom2.addEventListener('click', (e) => {
-    e.preventDefault();
-    const audio = new Audio('media/sounds/tom2.wav');
-    audio.play();
-})
-
-let ride = document.getElementById("ride");
-ride.addEventListener('click', (e) => {
-    e.preventDefault();
-    const audio = new Audio('media/sounds/ride.mp3');
-    audio.play();
-})
-
-let box = document.getElementById("box");
-box.addEventListener('click', (e) => {
-    e.preventDefault();
-    const audio = new Audio('media/sounds/box.wav');
-    audio.play();
-})
 
 //Función para emitir sonido al presionar una tecla
 function keydownSound(event) {
